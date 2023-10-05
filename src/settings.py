c917b1bd39ba814dda150a3598e532e8f84bfeb7
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-05 14:55:29 +0600

    hide - reason: copy of Supervisely Persons

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Person Segmentation"
 PROJECT_NAME_FULL: str = "Person Segmentation"
-HIDE_DATASET = False  # set False when 100% sure about repo quality
+HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
